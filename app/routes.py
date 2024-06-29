39ad79e8ad59c346b23650df689f9b8413051b17
Sampad Adhikary <sampadadhikary031@gmail.com>
2024-06-29 06:11:41 +0000

    minor

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,4 @@ def index():
     start_sniffing()
     print(ip_country_mapping)
     print(device_user_mapping)
-    return render_template('index.html', ip_country_mapping=ip_country_mapping, device_user_mapping=device_user_mapping)
+    return render_template('index.html')
